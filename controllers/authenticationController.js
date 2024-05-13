89163576c9893022b68e39425bcd49a419f56edb
manish-sharma-intimetec <manish.sharma@intimetec.com>
2024-05-13 23:54:07 +0530

    jwt

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,19 +1,39 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
-exports.signUp = async (request, response) => { 
-    // console.log(request.body); 
+exports.signUp = async (request, response) => {
+    // console.log(request.body);
 
     try {
         const newUser = await User.create(request.body);
+        const token = jwt.sign({id: newUser._id}, 'secret_key_123', {
+            expiresIn: '90d'
+        });
 
-    response.status(201).json({
-        status: 'success',
-        data: {
-            user: newUser
-        }
-    });
+        response.status(201).json({
+            status: 'success',
+            token,
+            data: {
+                user: newUser
+            }
+        });
     } catch (error) {
-        response.status(400).json({error: error.message});
+        response.status(400).json({ error: error.message });
     }
 }
 
+exports.signIn = async (request, response) => {
+
+    try {
+        const newUser = await User.findOne({ email: request.body.email, password: request.body.password }).select('-password -__v');
+
+        response.status(201).json({
+            status: 'success',
+            data: {
+                user: newUser
+            }
+        });
+    } catch (error) {
+        response.status(400).json({ error: error.message });
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 
 router.post('/signup', authenticationController.signUp);
+router.post('/signin', authenticationController.signIn);
 
 module.exports = router;
